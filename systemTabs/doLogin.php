9a9bae734dcd65be2c6679e0207ce65beae52a2a
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-08-28 01:26:42 -0300

    Translation to English, systemTabs file

---
+++ systemTabs/doLogin.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("dbConnection.php");
+
+
+$usuario = $_POST['usuario'];
+$senha = $_POST['senha'];
+
+$senha=md5($senha);
+
+// echo $usuario."    ".$senha;
+
+$comando = "SELECT * FROM usuarios WHERE
+usuario='".$usuario."' AND senha='".$senha."'";
+
+// echo $comando;
+$resultado=mysqli_query($conexao, $comando);
+$user = mysqli_fetch_assoc($resultado);
+$linhas=mysqli_num_rows($resultado);
+
+if ($linhas==0) {
+  header("Location: ../websiteTabs/login.php");
+}else{
+  session_start();
+  $_SESSION['idLogado'] = $user['id'];
+  $_SESSION['usuarioLogado'] = $user['usuario'];
+  $_SESSION['nivelLogado'] = $user['graupermissao'];
+
+  if($_SESSION['nivelLogado']==1){
+    header("Location: manager/home.php");
+  }else if($_SESSION['nivelLogado']==2){
+    header("Location: costumizer/home.php");
+  }else {
+    header("Location: attendant/home.php");
+  }
+}
+
+ ?>
