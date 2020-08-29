c82125c7afa246cc2e521edef20b4b682e35ab3a
Gabriel Duessmann <gabriel.duessmann@gmail.com>
2020-08-28 23:00:07 -0300

    Translation to English. Layout

--- systemTabs/customizer/customizerMenuLayout.php
+++ systemTabs/customizer/customizerMenuLayout.php
@@ -5,7 +5,7 @@
 		<ul id="menuPrincipal">
 			<a href="home.php" class="botaoClique"><img src="../../img/icone.png" id="logo" alt="icone"><li></li></a>
 				
-			<a href="workOrderEditionForm.php" class="botaoClique"><li><p>Ordem de Serviço</p></li></a>
+			<a href="workOrderEditionForm.php" class="botaoClique"><li><p>Work Order</p></li></a>
 			<li id="campoNome"><p id="nome"><?=$_SESSION['usuarioLogado'];?></p></li>
 			<a href="../doLogout.php"><img src="../../img/logout.jpg" id="botaoSair" alt="botão para deslogar o usuário"></a>
 		</ul>		
